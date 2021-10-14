b4a069582aafa116071282316ed87fe8c32d6d66
Gabriel Goiri <gabriel.goiri@rtgs.com>
2021-10-14 17:08:22 +0100

    added tests and functionality to getting products by tags

--- BCKataEcommerce.Test/UnitTest1.cs
+++ BCKataEcommerce.Test/UnitTest1.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using BCKataEcommerce;
 using BCKataEcommerce.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BCKataEcommerce.Test
 {
-    public class HttpRequestResponse
+    public class HttpRequestResponseTests
     {
         [Fact]
         public void Get_AllProducts()
@@ -15,20 +19,40 @@ namespace BCKataEcommerce.Test
             // Act
             var response = controller.Get();
             // Assert
-            Assert.NotNull(response);
+            var okResult = Assert.IsType<OkObjectResult>(response);
+            var returnValue = Assert.IsType<List<Product>>(okResult.Value);
+            returnValue.Count().Should().Be(3);
         }
 
-        [Theory]
-        [InlineData("Apple")]
-        [InlineData("aubergine")]
-        public void Get_ProductsByName(string name)
-        {
-            // Arrange
-            var controller = new ProductsController();
-            // Act
-            var response = controller.GetByName(name);
-            // Assert
-            Assert.NotNull(response);
-        }
+    [Theory]
+    [InlineData("Apple", 1)]
+    [InlineData("aubergine", 3)]
+    public void Get_ProductsByName(string name, int expectedId)
+    {
+        // Arrange
+        var controller = new ProductsController();
+        // Act
+        var response = controller.GetByName(name);
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        var returnValue = Assert.IsType<Product>(okResult.Value);
+        returnValue.Id.Should().Be(expectedId);
+    }
+
+    [Theory]
+    [InlineData("fruit", 2)]
+    [InlineData("vegetable", 1)]
+    [InlineData("fruit, yellow", 1)]
+    public void Get_ProductsByTags(string tags, int expectedCount)
+    {
+        // Arrange
+        var controller = new ProductsController();
+        // Act
+        var response = controller.GetByTags(tags);
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        var returnValue = Assert.IsType<List<Product>>(okResult.Value);
+            returnValue.Count.Should().Be(expectedCount);
+    }
     }
 }
